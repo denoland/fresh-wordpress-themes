4b67cc88a22df436bd741286bfbb212f6b279cfd
hashrock <hashrock@users.noreply.github.com>
2022-09-06 12:16:20 +0900

    Corporate template (#1)

---
+++ corporate/main.ts
@@ -0,0 +1,13 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+/// <reference lib="deno.unstable" />
+
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.js";
+
+await start(manifest, { plugins: [twindPlugin(twindConfig)] });
---
+++ corporate/twind.config.js
@@ -0,0 +1,14 @@
+/** @type {import("$fresh/plugins/twind.ts").Options} */
+export default {
+  theme: {
+    extend: {
+      width: {
+        md: "28rem",
+        xl: "36rem",
+      },
+      height: {
+        md: "28rem",
+      },
+    },
+  },
+};
