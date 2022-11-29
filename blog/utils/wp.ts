b3459660f353a884ef26a8ea170c99d2f67a0902
S.Y. Lee <shiyung+github@gmail.com>
2022-11-29 16:57:34 +0800

    feat: support wordpress.com website (#9)
    Co-authored-by: Yoshiya Hinosawa <stibium121@gmail.com>

--- blog/utils/wp.ts
+++ blog/utils/wp.ts
@@ -3,11 +3,27 @@
 import * as WP from "https://raw.githubusercontent.com/johnbillion/wp-json-schemas/2117b51650c9a662803e74182edf06af9bd327f7/packages/wp-types/index.ts";
 
 export { WP };
-const WP_API = Deno.env.get("WP_API") || "http://localhost/wp-json";
 
-if (!WP_API) {
-  throw Error("WP_API is not set");
+let WP_API = Deno.env.get("WP_API");
+const WP_COM_DOMAIN = Deno.env.get("WP_COM_DOMAIN");
+
+if (WP_COM_DOMAIN) {
+  console.log("Using WP_COM_DOMAIN=" + WP_COM_DOMAIN);
+} else if (WP_API) {
+  console.log("Using WP_API=" + WP_API);
+} else {
+  console.log(
+    "WP_API and WP_COM_DOMAIN env vars are not set. Using default WP_API (http://localhost/wp-json)",
+  );
+  WP_API = "http://localhost/wp-json";
 }
+
+const WP_API_ROOT = WP_COM_DOMAIN
+  ? `https://public-api.wordpress.com/wp/v2/sites/${WP_COM_DOMAIN}`
+  : WP_API + "/wp/v2";
+const SITE_INFO_API = WP_API
+  ? WP_API
+  : `https://public-api.wordpress.com/rest/v1.1/sites/${WP_COM_DOMAIN}`;
 
 export type WpPost = WP.WP_REST_API_Post;
 export type WpCategory = WP.WP_REST_API_Category;
@@ -29,7 +45,7 @@ export async function callApi<T = unknown>(
   path: string,
   options?: RequestInit,
 ): Promise<[T, WpResponseMetadata]> {
-  const resp = await fetch(WP_API + path, options);
+  const resp = await fetch(WP_API_ROOT + path, options);
   return [await resp.json() as T, {
     total: toNum(resp.headers.get("x-wp-total")),
     totalPages: toNum(resp.headers.get("x-wp-totalpages")),
@@ -38,14 +54,14 @@ export async function callApi<T = unknown>(
 }
 
 export async function getSiteName() {
-  const [{ name }] = await callApi<{ name: string }>("/?fields=name");
-  return name;
+  const resp = await fetch(SITE_INFO_API + "/?fields=name");
+  return (await resp.json())?.name || "Untitled";
 }
 
 /** Gets all pages */
 export async function getPages(): Promise<WpPost[]> {
   const [pages] = await callApi<WpPost[]>(
-    "/wp/v2/pages?per_page=100&orderby=menu_order&order=asc",
+    "/pages?per_page=100&orderby=menu_order&order=asc",
   );
   return pages;
 }
@@ -56,7 +72,7 @@ const listQuery = "_embed=wp:featuredmedia";
 export function getPosts(
   page: number,
 ): Promise<[WpPost[], WpResponseMetadata]> {
-  const path = `/wp/v2/posts?page=${page}&${listQuery}`;
+  const path = `/posts?page=${page}&${listQuery}`;
   return callApi<WpPost[]>(path);
 }
 
@@ -65,8 +81,7 @@ export function getPostsByCategoryId(
   page: number,
   categoryId: number,
 ): Promise<[WpPost[], WpResponseMetadata]> {
-  const path =
-    `/wp/v2/posts?page=${page}&categories=${categoryId}&${listQuery}`;
+  const path = `/posts?page=${page}&categories=${categoryId}&${listQuery}`;
   return callApi<WpPost[]>(path);
 }
 
@@ -75,7 +90,7 @@ export function getPostsByTagId(
   page: number,
   tagId: number,
 ): Promise<[WpPost[], WpResponseMetadata]> {
-  const path = `/wp/v2/posts?page=${page}&tags=${tagId}&${listQuery}`;
+  const path = `/posts?page=${page}&tags=${tagId}&${listQuery}`;
   return callApi<WpPost[]>(path);
 }
 
@@ -83,33 +98,33 @@ export function getPostsByTagId(
 export async function getCategoryBySlug(
   slug: string,
 ): Promise<WpCategory | undefined> {
-  const [categories] = await callApi<WpTag[]>(`/wp/v2/categories?slug=${slug}`);
+  const [categories] = await callApi<WpTag[]>(`/categories?slug=${slug}`);
   return categories[0];
 }
 
 /** Gets the tag of the given slug */
 export async function getTagBySlug(slug: string): Promise<WpTag | undefined> {
-  const [tags] = await callApi<WpTag[]>(`/wp/v2/tags?slug=${slug}`);
+  const [tags] = await callApi<WpTag[]>(`/tags?slug=${slug}`);
   return tags[0];
 }
 
 /** Gets the sticky post if exists */
 export async function getStickyPost(): Promise<WpPost | undefined> {
-  const path = `/wp/v2/posts?sticky=1&${listQuery}`;
+  const path = `/posts?sticky=1&${listQuery}`;
   const [posts] = await callApi<WpPost[]>(path);
   return posts[0];
 }
 
 /** Gets the post by the give slug */
 export async function getPostBySlug(slug: string): Promise<WpPost | undefined> {
-  const path = `/wp/v2/posts?slug=${slug}&_embed=author,wp:term,replies`;
+  const path = `/posts?slug=${slug}&_embed=author,wp:term,replies`;
   const [posts] = await callApi<WpPost[]>(path);
   return posts[0];
 }
 
 /** Gets the post by the give slug */
 export async function getPageBySlug(slug: string): Promise<WpPost | undefined> {
-  const path = `/wp/v2/pages?slug=${slug}&_embed=author,wp:term`;
+  const path = `/pages?slug=${slug}&_embed=author,wp:term`;
   const [posts] = await callApi<WpPost[]>(path);
   return posts[0];
 }
@@ -123,7 +138,7 @@ type PostCommentParams = {
 
 /** Posts an comment to the given post */
 export async function postComment(params: PostCommentParams) {
-  const path = `/wp/v2/comments`;
+  const path = `/comments`;
   const body = JSON.stringify({
     author_name: params.name,
     author_email: params.email,
